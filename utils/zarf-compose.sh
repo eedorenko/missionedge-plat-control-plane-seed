e7f282db95b3fb190c83db79394ac7bf28e97994
vyta <vvyy.ta@outlook.com>
2022-05-09 17:50:14 -0600

    Add manifest components first (#18)

--- utils/zarf-compose.sh
+++ utils/zarf-compose.sh
@@ -1,13 +1,21 @@
 script_path=`dirname $(readlink -f $0)`
+manifest_components_filename=manifest_components.yaml
+app_components_filename=app_components.yaml
 
-# render_zarf_file CLUSTER_NAME CLUSTER_FOLDER 
+# render_zarf_file CLUSTER_NAME
 render_zarf_file() {
-   sed 's#{CLUSTER_NAME}#'$1'#g' $script_path/templates/zarf-header.yaml > $2/zarf.yaml
+   sed 's#{CLUSTER_NAME}#'$1'#g' $script_path/templates/zarf-header.yaml > zarf.yaml
+   if [ -f $manifest_components_filename ]; then
+    cat $manifest_components_filename >> zarf.yaml
+   fi
+   if [ -f $app_components_filename ]; then
+    cat $app_components_filename >> zarf.yaml
+   fi
 }
 
-# add_component COMPONENT_NAME COMPONENT_PAT
+# add_component COMPONENT_NAME COMPONENT_PATH COMPONENT_FILE
 add_component() {
-   sed 's#{COMPONENT_NAME}#'$1'#g; s#{COMPONENT_PATH}#'$2'#g' $script_path/templates/zarf-component.yaml >> zarf.yaml
+   sed 's#{COMPONENT_NAME}#'$1'#g; s#{COMPONENT_PATH}#'$2'#g' $script_path/templates/zarf-component.yaml >> $3
 }
 
 # compose_cluster CLUSTER_NAME
@@ -16,28 +24,35 @@ compose_cluster() {
    root_cluster_folder=./clusters/$cluster_name
    flux_system_kustomization=./flux-system/kustomization.yaml
 
-   render_zarf_file $cluster_name $root_cluster_folder
-
    cd $root_cluster_folder
+   touch $manifest_components_filename
+   touch $app_components_filename
 
    for app in `find . -name kustomization.yaml -not -path $flux_system_kustomization `; do
-   ws_name=$(echo $app | cut -d/ -f2) 
-   app_type=$(echo $app | cut -d/ -f3) 
-   app_name=$(echo $app | cut -d/ -f4) 
+      ws_name=$(echo $app | cut -d/ -f2) 
+      app_type=$(echo $app | cut -d/ -f3) 
+      app_name=$(echo $app | cut -d/ -f4) 
 
-   app_path=../../applications/$ws_name/$app_type/$app_name
+      app_path=../../applications/$ws_name/$app_type/$app_name
 
-   if [ $app_type == 'ApplicationRegistrations' ]
-   then
-      app_target=$(echo $app | cut -d/ -f5)
-      app_path=$app_path/$app_target
-   fi 
+      if [ $app_type == 'ApplicationRegistrations' ]
+      then
+          app_target=$(echo $app | cut -d/ -f5)
+          app_path=$app_path/$app_target
+      fi 
 
-   if [ -f $app_path/zarf.yaml ]; then
-     add_component $app_name $app_path
-   fi 
+      if [ -f $app_path/zarf.yaml ]; then
+        if [ $app_type == 'ManifestDeployments' ]; then
+          add_component $app_name $app_path $manifest_components_filename
+        else
+          add_component $app_name $app_path $app_components_filename
+        fi
+      fi 
    
    done
+   
+   render_zarf_file $cluster_name
+   rm $manifest_components_filename $app_components_filename
    cd -
 }
 
